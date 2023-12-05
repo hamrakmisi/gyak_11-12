c51981c04a99b20fde943a620ac46a224e29197a
hamrakmisi <misi@hamy.hu>
2023-12-05 08:57:37 +0100

    mentes

---
+++ gyak_11/Controllers/Boatcontroller.cs
@@ -0,0 +1,21 @@
+﻿using gyak_11.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gyak_11.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class Boatcontroller : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
